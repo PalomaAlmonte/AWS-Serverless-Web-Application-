b92eb68ed9050cc07b3f6212ae1bb27e26fdcc20
paloma <palomaalmontez@gmail.com>
2023-12-19 13:36:27 -0500

    new_config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_iV85mxuMX', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1las4jmq79h0fbj1pb0nqv7213', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
